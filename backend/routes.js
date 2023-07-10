dd0575975697b550ecbb03ff1ff18d8857ac6256
levius_codeWell <taskaleg@gmail.com>
2023-07-09 19:14:48 -0500

    updated router handlers. able to post user and retrieve users.

--- backend/routes.js
+++ backend/routes.js
@@ -4,18 +4,29 @@ const router = express.Router();
 let users = [];
 
 router.post('/register', (req, res) => {
-  const { username, password, name, college, yearOfGradduation } = req.body;
+  const { username, password, name, college, yearOfGraduation } = req.body;
 
   const existingUser = users.find(user => user.username === username);
   if (existingUser) {
     return res.status(400).json({ error: 'Username already exists' });
-  }
+  } 
 
-  const user = { username, password, name, college, yearOfGradduation };
+  const user = { username, password, name, college, yearOfGraduation };
 
   users.push(user);
 
-  res.status(201).json(user, { message: 'User created successfully' });
-})
+  res.status(201).json({ message: 'User created successfully' });
+  
+ console.log(users);
+});
+
+router.get('/users', (req, res) => {
+    const getUsers = users.map(user => {
+        const { password, ...usersWithoutPw } = user;
+        return usersWithoutPw;
+    })
+    res.json(getUsers);
+});
+
 
 module.exports = router;
