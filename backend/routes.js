f5eb968893800fc10bb57e8980278532a0957434
levius_codeWell <taskaleg@gmail.com>
2023-07-10 18:13:23 -0500

    front end form added

--- backend/routes.js
+++ backend/routes.js
@@ -29,4 +29,5 @@ router.get('/users', (req, res) => {
 });
 
 
+
 module.exports = router;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Button, Card, CardContent } from '@material-ui/core';
+import { Link, Route, Routes } from 'react-router-dom';
+import Register from './components/register';
+import UpdateProfile from './components/updateProfile';
+import DisplayProfiles from './components/displayProfile';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Link to="/" style={{ textDecoration: "none" }}>
+        <Button style={{ display: "flex", marginLeft: "auto", marginRight: "auto", marginTop: "20px" }} color="primary" variant="contained">Home</Button>
+      </Link>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/update" element={<UpdateProfile />} />
+        <Route path="/all" element={<DisplayProfiles />} />
+        <Route path="/" element={
+          <div style={{ margin: "auto", height: "100%", display: "flex", flexDirection: "column" }}>
+            <Card style={{ display: "flex", flexDirection: "column", marginLeft: 'auto', marginRight: 'auto', margin: 'auto' }}>
+              <CardContent style={{ display: "flex", flexDirection: "column" }}>
+                <Link style={{ minWidth: "200px" }} to="/register">
+                  <Button color="primary" variant="outlined" style={{ margin: "10px", minWidth: "200px" }}>
+                    Register
+                  </Button>
+                </Link>
+                <Link style={{ minWidth: "200px" }} to="/all">
+                  <Button color="primary" variant="outlined" style={{ margin: "10px", minWidth: "200px" }}>
+                    View all Profiles
+                  </Button>
+                </Link>
+                <Link style={{ minWidth: "200px" }} to="/update">
+                  <Button color="primary" variant="outlined" style={{ margin: "10px", minWidth: "200px" }}>
+                    Update your Profile
+                  </Button>
+                </Link>
+              </CardContent>
+            </Card>
+          </div>
+        } />
+      </Routes>
+    </>
   );
 }
-
-export default App;
